0a5628699371fd801c2b02de398fd2bdd93cae00
Lukas Jaiczay <lukas.jaiczay@stud.fh-campuswien.ac.at>
2024-10-11 17:08:35 +0200

    new Message

--- DoSomethingClass.java
+++ DoSomethingClass.java
@@ -1,5 +1,6 @@
 public class DoSomethingClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Diese Nachricht kommt in meinem Jenkis aufruf an");
     }
 }
